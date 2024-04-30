4c52be5c63fa41de009eb8f29b49f0df1c670d86
Gemma <jemmimah.abdul@decagon.dev>
2024-04-30 07:12:59 +0100

    Add project files.

---
+++ movieProject/Controllers/MovieController.cs
@@ -0,0 +1,67 @@
+﻿using Data.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using movieProject.Model;
+using movieProject.MovieServices;
+using Newtonsoft.Json;
+
+namespace movieProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private  readonly IMovieService _movieService;
+
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+
+        [HttpGet("search/{title}")]
+        public async Task<IActionResult> MovieSearch(string title)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(title))
+                {
+                    return BadRequest("Please supply a valid movie name");
+                } 
+                var result = await _movieService.MovieSearchAsync(title);
+                return Ok(JemmimahApiResponse.Success(result));
+            }
+            catch(Exception ex) 
+            {
+                return BadRequest("An error occured while fetching your movie.");
+            }                         
+        }
+
+
+
+
+
+        [HttpGet("searchHistory")]
+        public async Task<IActionResult> GetSearchHistory()
+        {
+            try
+            {
+                var search = await _movieService.GetSearchHistoryAsync();
+                if(search == null)
+                {
+                    return NotFound(JemmimahApiResponse.Failed("No recent searches found."));
+                }
+                return Ok(JemmimahApiResponse.Success(search));
+            }
+            catch(Exception ex) 
+            {
+                return BadRequest("Oops, Please Try again later.");
+            }
+        }
+
+
+
+
+
+    }
+}
